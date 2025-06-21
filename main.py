aaa6befba7e6ccf9d09e29cdfb43f000fa8d6b8f
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-06-21 15:33:57 +0200

    Remove unused imports

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 # main.py
 import os
 import json
-import uuid
 import threading
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog, simpledialog
 from datetime import datetime
 import requests
 from minecraft_launcher_lib.utils import get_minecraft_directory
-from utils import download_file_with_progress,ensure_requirements, install_modpack_files, check_update, update_modpack_info, install_forge_if_needed, update_installed_info, refresh_ms_token, exchange_code_for_token, authenticate_with_xbox, authenticate_with_xsts, login_with_minecraft, get_minecraft_profile
+from utils import ensure_requirements, install_modpack_files, check_update, update_modpack_info, install_forge_if_needed, update_installed_info, refresh_ms_token, exchange_code_for_token, authenticate_with_xbox, authenticate_with_xsts, login_with_minecraft, get_minecraft_profile
 from minecraft_launcher_lib.command import get_minecraft_command
-import sys
 import subprocess
-import importlib
 import functools
 from urllib.parse import urlparse, parse_qs
 
