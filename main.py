1ba1bb032091be55b793052dbff4dd23b260c75c
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-06-22 01:16:31 +0200

    Add modpack

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from src.launcher import MinecraftLauncher
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MinecraftLauncher()
-    window.show()
-    sys.exit(app.exec_())
