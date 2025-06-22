b34d75c61cd36898abcb8485593c680e9c9ae23a
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-06-22 16:54:54 +0200

    Add logo but not work

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 from src.launcher import MinecraftLauncher
 
 def main():
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("assets/textures/logo.ico"))
     window = MinecraftLauncher()
     window.show()
     sys.exit(app.exec_())
